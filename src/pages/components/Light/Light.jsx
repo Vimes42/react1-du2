bf1961a87a0ef37c75b26cbcf73a8816ce2f09ff
Lenka Polcerova <lenka.polcerova@gmail.com>
2024-10-12 15:37:12 +0200

    Lights - dořešeno css

--- src/pages/components/Light/Light.jsx
+++ src/pages/components/Light/Light.jsx
@@ -1,6 +1,7 @@
 import lightoff from './images/light-off.svg';
 import lighton from './images/light-on.svg';
 import React, {useState} from 'react';
+import './Light.css';
 
 const Light = ({name, state}) => {
     const [stateLights, setStateLights] = useState({state});
